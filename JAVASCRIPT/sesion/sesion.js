bd9f1393dec5a7b37b3cbf6695d8e832fe446f86
= <=>
2024-01-12 14:13:35 +0100

    Update

--- JAVASCRIPT/sesion/sesion.js
+++ JAVASCRIPT/sesion/sesion.js
@@ -1,5 +1,5 @@
 function inicio_sesion(){
-    var user = document.getElementById("usuario");
-    var passwd = document.getElementById("contraseña");
+    let user = document.getElementById("usuario");
+    let passwd = document.getElementById("contraseña");
     alert = "Bienvenido usuario " + user + "con contraseña " + passwd;
 }
